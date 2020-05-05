0f9656bd948d3cb3d99a9b47c5a19c77c538ed91
jwo78 <jwo78@uclive.ac.nz>
2020-05-05 13:47:36 +1200

    Added the entity for transaction

--- app/src/main/java/com/con19/tripplanner/entities/Transaction.kt
+++ app/src/main/java/com/con19/tripplanner/entities/Transaction.kt
@@ -1,3 +1,23 @@
 package com.con19.tripplanner.entities
 
-class Transaction() {}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "transactions")
+class Transaction(
+    @ColumnInfo(name = "name")
+    var transactionName: String,
+    @ColumnInfo(name = "creation_date")
+    var creationDate: Date,
+    var cost: Int,
+    var image: String,
+    var members: MutableList<Person>,
+    @ColumnInfo(name = "split_status")
+    var splitStatus: Boolean
+) {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long = 0
+}
